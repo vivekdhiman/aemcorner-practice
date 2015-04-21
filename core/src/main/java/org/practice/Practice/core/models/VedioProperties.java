ce341a52536f092b7cd3e496b149e92c04f9e45d
vivek dhiman <vivek.dhiman@intelligrape.com>
2015-04-21 11:35:06 +0530

    Intial Commit

---
+++ core/src/main/java/org/practice/Practice/core/models/VedioProperties.java
@@ -0,0 +1,44 @@
+package org.practice.Practice.core.models;
+
+import java.util.Date;
+
+public class VedioProperties {
+    private String videoId;
+    private String title;
+    private String thumbnail;
+    private String publishDate;
+    public String getPublishDate() {
+        return publishDate;
+    }
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+    public String getVideoId() {
+        return videoId;
+    }
+    public void setVideoId(String videoId) {
+        this.videoId = videoId;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getThumbnail() {
+        return thumbnail;
+    }
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+    public VedioProperties(String videoId, String title, String thumbnail,String publishDate) {
+        super();
+        this.videoId = videoId;
+        this.title = title;
+        this.thumbnail = thumbnail;
+        this.publishDate = publishDate;
+
+    }
+
+
+}
---
+++ ui.apps/src/main/content/jcr_root/apps/practice/components/content/simpletext/simpletext.jsp
@@ -0,0 +1,14 @@
+<%--
+
+  Simple Text component.
+
+  
+
+--%><%
+%><%@include file="/libs/foundation/global.jsp"%><%
+%><%@page session="false" %><%
+%><%
+	// TODO add you code here
+%>
+
+Hello ${properties['text']}
